6c6f21b35d8d1bced18e94b532e30f369ccc43b1
Aqib-Web76 <aqibwebdesign@gmail.com>
2024-05-28 03:39:26 -0700

    query selcector

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
 
-let header = document.getElementById("header");
-console.log(header);
+// let heading = document.getElementsByClassName("heading");
+// console.dir(heading);
+// console.log(heading);
+// let heading = document.querySelector(".heading");
+// console.dir(heading);
+// let btn = document.querySelector(".btn");
+// console.dir(btn);
+let header = document.getElementsByTagName(".btn");
+console.dir(header);

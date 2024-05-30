7463f6ee73aacd3d257484bacad8e11ef86870cf
Aqib-Web76 <aqibwebdesign@gmail.com>
2024-05-30 01:53:05 -0700

    Event Handling

--- dom.js
+++ dom.js
@@ -60,14 +60,70 @@ for ( let div of d) {
 // let para = document.querySelector("p");
 // para.remove();
 
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click Me";
-console.log(newBtn);
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click Me";
+// console.log(newBtn);
 
-let div = document.querySelector("div");
-div.before
-    (newBtn);
-let btn = document.querySelector("button");
-console.log(btn);
-newBtn.style.backgroundColor = "red";
-newBtn.style.color = "white";
+// let div = document.querySelector("div");
+// div.before
+//     (newBtn);
+// let btn = document.querySelector("button");
+// console.log(btn);
+// newBtn.style.backgroundColor = "red";
+// newBtn.style.color = "white";
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = (evt) => {
+//     console.log(evt);
+//     console.log(evt);
+//     console.log(evt.type, evt.target);
+//     console.log("btn1 was click");
+//     let a = 12;
+//     a++;
+//     console.log(a);
+// }
+
+// let div = document.querySelector("div");
+// box.onclick = () => {
+//     console.log("the div is clicked");
+// }
+
+/*event lisner*/
+// let btn1 = document.querySelector("#btn1");
+ 
+// btn1.addEventListener("click", () => {
+//     console.log("button1 is clicked");
+// })
+ 
+// btn1.addEventListener("click", () => {
+//     console.log("button1 is clicked second time ");
+  
+// })
+
+ 
+// const handler3 = () => {
+//     console.log("button1 is clicked third time");
+
+// }
+// btn1.addEventListener("click", handler3);
+ 
+// btn1.addEventListener("click", () => {
+//     console.log("button1 is clicked fourth time")
+//     alert("button 3 is removed");
+// })
+ 
+// btn1.removeEventListener("click", handler3);
+let modeBtn = document.querySelector("#mode");
+
+let currMode = "light";
+
+modeBtn.addEventListener("click", () => {
+    if (currMode === "light") {
+        currMode = "dark";
+        document.querySelector("body").style.backgroundColor = " black";
+    } else {
+        currMode = "light";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+    console.log(currMode);
+})
